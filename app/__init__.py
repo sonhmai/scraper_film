8efd4cfb1b24723baac35136489c91bda41aacc1
Son Mai <hongson1104@gmail.com>
2019-04-22 14:39:22 +0700

    add migrations and move db to proj root folder:

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,16 @@
+import os
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+db_path = 'sqlite:///' + os.path.dirname(os.path.dirname(__file__)) + '/app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = db_path
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 from app.api.movies import bp as api_blueprint
 
 from app.models import Movie
--- app/models.py
+++ app/models.py
@@ -1,6 +1,11 @@
 from app import db
 
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String, unique=True)
+
+
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String, unique=True)
